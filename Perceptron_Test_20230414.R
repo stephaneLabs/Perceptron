8275fd1dc29e1b177c0136f3ea3cc0d529e9ba11
Stéphane L <98824978+stephaneLabs@users.noreply.github.com>
2023-04-14 22:16:56 +0200

    Add files via upload

---
+++ Perceptron_Test_20230414.R
@@ -0,0 +1,84 @@
+# This is free and unencumbered software released into the public domain.
+# 
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+# 
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+# 
+# For more information, please refer to <http://unlicense.org/>
+
+#' References :
+#' - Generalization of the code available on the website https://www.databricks.com/ to use more quantitative predictors and encapsulation within a function, 
+#' Source material including a data simulation is from this page :
+#' https://databricks-prod-cloudfront.cloud.databricks.com/public/4027ec902e239c93eaaa8714f173bcfc/2961012104553482/2761297084239405/1806228006848429/latest.html
+#'
+#' - Cheng, B., & Titterington, D. M. (1994). Neural Networks: A Review from a Statistical Perspective. Statistical Science, 9(1), 2–30. http://www.jstor.org/stable/2246275
+#' - Rosenblatt, F. (1958). The perceptron: a probabilistic model for information storage and organization in the brain. Psychological review, 65(6), 386.
+#'
+#'
+library(tidyverse)
+# source("Perceptron_function_20230414.R")
+set.seed(4567)
+
+N = 50 # total number of data points each group
+x_offset = 0.5 # group separation on x axis
+y_offset = 0.5 # group separation on y axis
+
+g1_x = runif(N, min = 0, max = 1)
+g1_y = runif(N, min = 0, max = 1)
+
+g2_x = runif(N, min = 0+x_offset, max = 1+x_offset)
+g2_y = runif(N, min = 0+y_offset, max = 1+y_offset)
+
+g_x = c(g1_x, g2_x)
+g_y = c(g1_y, g2_y)
+y = c(rep(-1,N), rep(1,N))
+
+print(g_x)
+print(g_y)
+print(y)
+
+X <- cbind(x = g_x, y = g_y)
+
+# Computation
+results <- perceptron(y, X, eta = 0.05, batch_size = trunc(nrow(X)/2), verbose = TRUE)
+
+# Plot with the observed classes
+par(mfrow=c(1,2))
+plot(g_x, g_y, type='n', xlab='X', ylab='Y', main = "Scatterplot of predictors\nwith the initial classes")
+points(g1_x, g1_y, col='red')
+points(g2_x, g2_y, col='blue')
+
+# second part of the plot with classes computed by the model
+colors.vec <- results$fitted
+colors.vec[colors.vec == -1] <- 0
+colors.vec <- colors.vec + 1
+plot(g_x, g_y, type='n', xlab='X', ylab='Y', main = "Scatterplot of predictors\nwith the predicted classes")
+points(c(g1_x,g2_x), c(g1_y, g2_y), col=c("burlywood","green")[colors.vec])
+
+# Confusion matrix
+conf_matrix <- table(fitted = results$fitted, initial = y)
+conf_matrix
+
+# Sensibility
+sens <- conf_matrix[1,1] / sum(conf_matrix[,1])
+sens
+# Specificity
+spec <- conf_matrix[2,2] / sum(conf_matrix[,2])
+spec
